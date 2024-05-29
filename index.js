828adf187b708e05142bb19bd184d8d748eb8b45
TEO <alfredo.ssm@gmail.com>
2024-05-28 21:28:22 -0400

    Se deja funcional

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-precio = 400000
+const precio = 400000
+let cantidadInicial = 0
+let valorTotalInicial = 0
+const precioSpan = document.querySelector('.precio-inicial')
+const valorTotal = document.querySelector('.valor-total')
+const cantidadSpan = document.querySelector('.cantidad')
+const botonRestar = document.querySelector('.restar')
+const botonSumar = document.querySelector('.sumar')
 
-precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+cantidadSpan.innerHTML = cantidadInicial
+valorTotal.innerHTML = valorTotalInicial
+
+botonRestar.addEventListener('click', () => {
+  console.log('click restar');
+  if (cantidadInicial > 0) {
+    cantidadInicial--
+    cantidadSpan.innerHTML = cantidadInicial
+    valorTotal.innerHTML = precio * cantidadInicial
+  }
+})
+
+botonSumar.addEventListener('click', () => {
+  console.log('click sumar');
+  cantidadInicial++
+  cantidadSpan.innerHTML = cantidadInicial
+  valorTotal.innerHTML = precio * cantidadInicial
+})
